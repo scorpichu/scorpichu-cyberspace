a0a1e9ca98f4ebbcdb0b9d27277c8d24d3235674
scorpichu <chiunaomi@gmail.com>
2026-02-15 03:38:39 +0800

    Add Next redirect from /blog to redesigned static page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: '/pages/scorpiblog.html',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
